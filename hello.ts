425564211661466b4aac2dfbba5d2710538dcb4e
syuhas <syuhas22@gmail.com>
2023-12-03 10:17:20 -0500

    init

---
+++ hello.ts
@@ -0,0 +1,12 @@
+
+//simple hello world typescript program, this will compile to essentialy the same thing as the hello.js file
+function hw(Hello: string) {
+    console.log(Hello);
+}
+var greeting = "Hello World";
+
+hw(greeting);
+
+
+
+
